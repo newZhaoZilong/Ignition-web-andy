c6e600f82aed529054102d4b54a1164607ecd1f9
赵青山 <>
2024-02-27 00:56:41 +0800

    feat: project init

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    env: {
+        node: true,
+        es2021: true,
+        browser: true
+    },
+            extends: [
+                'standard',
+                'standard-jsx'
+            ],
+            parser: '@typescript-eslint/parser',
+            rules: {
+                // 自定义规则
+                'no-unused-vars': 'off',
+                'no-useless-constructor': 'off'
+            }
+}
